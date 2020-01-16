97dead5cc428917dfa85b4892476be5e48741c18
Reza <dehghan7447@gmail.com>
2020-01-16 14:53:23 +0330

    repositories

--- src/main/java/ir/maktab/project12/instagram/repositories/PostRepository.java
+++ src/main/java/ir/maktab/project12/instagram/repositories/PostRepository.java
@@ -1,4 +1,10 @@
 package ir.maktab.project12.instagram.repositories;
 
-public class PostRepository {
+import ir.maktab.project12.instagram.entities.Post;
+
+public class PostRepository extends CrudRepository<Post,Long> {
+    @Override
+    protected Class<Post> getEntityClass() {
+        return Post.class;
+    }
 }
--- src/main/java/ir/maktab/project12/instagram/repositories/UserRepository.java
+++ src/main/java/ir/maktab/project12/instagram/repositories/UserRepository.java
@@ -1,4 +1,10 @@
 package ir.maktab.project12.instagram.repositories;
 
-public class UserRepository {
+import ir.maktab.project12.instagram.entities.User;
+
+public class UserRepository extends CrudRepository<User,Long> {
+    @Override
+    protected Class<User> getEntityClass() {
+        return User.class;
+    }
 }
